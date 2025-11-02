d595748be6edd163d5238016ec6e74d870d04c28
Michael Haynie <michael.dean.haynie@gmail.com>
2025-11-02 08:14:42 -0700

    fix b2d crash (probably) by NOT updating game object position during render()

--- src/littlejsengine/box2d/box2d-object-adapter/box2d-object-adapter-factory.al.ts
+++ src/littlejsengine/box2d/box2d-object-adapter/box2d-object-adapter-factory.al.ts
@@ -1,3 +1,4 @@
+import { inject } from "inversify";
 import { Autoloadable } from "../../../core/autoload/autoloadable";
 import type { Box2dObject } from "../../littlejsengine.types";
 import { Box2dObjectAdapter } from "./box2d-object-adapter";
@@ -6,14 +7,22 @@ import {
   type IBox2dObjectAdapterFactory,
 } from "./box2d-object-adapter-factory.types";
 import type { IBox2dObjectAdapter } from "./box2d-object-adapter.types";
+import type { ILJS } from "../../littlejsengine.impure";
+import { LJS_TOKEN } from "../../littlejsengine.token";
 
 @Autoloadable({
   serviceIdentifier: BOX2D_OBJECT_ADAPTER_FACTORY_TOKEN,
 })
 export class Box2dObjectAdapterFactory implements IBox2dObjectAdapterFactory {
+  private readonly _ljs: ILJS;
+
+  constructor(@inject(LJS_TOKEN) ljs: ILJS) {
+    this._ljs = ljs;
+  }
+
   createBox2dObjectAdapter(
     ...args: ConstructorParameters<typeof Box2dObject>
   ): IBox2dObjectAdapter {
-    return new Box2dObjectAdapter(...args);
+    return new Box2dObjectAdapter(this._ljs, ...args);
   }
 }
--- src/littlejsengine/box2d/box2d-object-adapter/box2d-object-adapter.ts
+++ src/littlejsengine/box2d/box2d-object-adapter/box2d-object-adapter.ts
@@ -2,12 +2,15 @@ import { Subject } from "rxjs";
 import type { IBox2dObjectAdapter } from "./box2d-object-adapter.types";
 import { Box2dObject } from "littlejsengine";
 import { vec2 } from "../../littlejsengine.pure";
+import type { ILJS } from "../../littlejsengine.impure";
 
 /** Default adapter used in the real app. Callbacks can be assigned for onUpdate etc. */
 export class Box2dObjectAdapter
   extends Box2dObject
   implements IBox2dObjectAdapter
 {
+  private readonly _ljs: ILJS;
+
   private _update$ = new Subject<void>();
   public update$ = this._update$.asObservable();
 
@@ -17,8 +20,9 @@ export class Box2dObjectAdapter
   // probably in world/screen space unis?
   public travelingHeight = 0;
 
-  constructor(...args: ConstructorParameters<typeof Box2dObject>) {
+  constructor(ljs: ILJS, ...args: ConstructorParameters<typeof Box2dObject>) {
     super(...args);
+    this._ljs = ljs;
   }
 
   override update(): void {
@@ -28,9 +32,20 @@ export class Box2dObjectAdapter
 
   override render(): void {
     this._render$.next();
+    // note: coppied from default impl - just updated the pos argument
+    this._ljs.drawTile(
+      this.pos.add(vec2(0, this.travelingHeight)),
+      this.drawSize || this.size,
+      this.tileInfo,
+      this.color,
+      this.angle,
+      this.mirror,
+      this.additiveColor,
+    );
     // michael: find a better solution than overwriting the position of the engine object for a hot second during rendering
-    this.setPosition(this.pos.add(vec2(0, this.travelingHeight)));
-    super.render();
-    this.setPosition(this.pos.subtract(vec2(0, this.travelingHeight)));
+    // I think the below was causing box2d to crash
+    // this.setPosition(this.pos.add(vec2(0, this.travelingHeight)));
+    // super.render();
+    // this.setPosition(this.pos.subtract(vec2(0, this.travelingHeight)));
   }
 }
